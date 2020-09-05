efabcec1668430d0c9e4a4bf714d1dfdfa74ba07
mhdhassan99 <mhassan.work99@gmail.com>
2020-09-05 11:40:33 -0400

    jjj

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  resources :products
+  resources :orders
+  resources :carts
+  resources :reviews
+  resources :users
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/logged_in', to: 'sessions#is_logged_in?'
+  post '/deleteCartItem', to: 'orders#delete_cart_item'
+  delete '/deleteAllCartItems', to: 'orders#delete_all_cart_items'
+  get '/trendingProducts', to: 'products#trending_products'
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
